070f8cad494de7a0672f1558149f1474940398c4
redplayer0 <mr_red83@protonmail.com>
2024-07-01 09:05:28 +0300

    changed dap ui

--- init.lua
+++ init.lua
@@ -60,8 +60,8 @@ set('n', ']d', vim.diagnostic.goto_next)
 set('n', '[d', vim.diagnostic.goto_prev)
 
 -- These mappings control the size of splits (height/width)
-set('n', '<M-,>', '<c-w>5<')
-set('n', '<M-.>', '<c-w>5>')
+set('n', '<M-.>', '<c-w>5<')
+set('n', '<M-,>', '<c-w>5>')
 set('n', '<M-t>', '<C-W>+')
 set('n', '<M-s>', '<C-W>-')
 
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,7 +11,89 @@ return {
       local dap = require 'dap'
       local ui = require 'dapui'
 
-      require('dapui').setup()
+      require('dapui').setup {
+        controls = {
+          element = 'repl',
+          enabled = false,
+          icons = {
+            disconnect = '',
+            pause = '',
+            play = '',
+            run_last = '',
+            step_back = '',
+            step_into = '',
+            step_out = '',
+            step_over = '',
+            terminate = '',
+          },
+        },
+        element_mappings = {},
+        expand_lines = true,
+        floating = {
+          border = 'single',
+          mappings = {
+            close = { 'q', '<Esc>' },
+          },
+        },
+        force_buffers = true,
+        icons = {
+          collapsed = '',
+          current_frame = '',
+          expanded = '',
+        },
+        layouts = {
+          {
+            elements = {
+              {
+                id = 'scopes',
+                size = 0.4,
+              },
+              {
+                id = 'breakpoints',
+                size = 0.2,
+              },
+              {
+                id = 'stacks',
+                size = 0.2,
+              },
+              {
+                id = 'watches',
+                size = 0.2,
+              },
+            },
+            position = 'left',
+            size = 60,
+          },
+          {
+            elements = {
+              -- {
+              --   id = 'repl',
+              --   size = 0.5,
+              -- },
+              {
+                id = 'console',
+                size = 1,
+              },
+            },
+            position = 'bottom',
+            size = 10,
+          },
+        },
+        mappings = {
+          edit = 'e',
+          expand = { '<CR>', '<2-LeftMouse>' },
+          open = 'o',
+          remove = 'd',
+          repl = 'r',
+          toggle = 't',
+        },
+        render = {
+          indent = 1,
+          max_value_lines = 100,
+        },
+      }
+
+      require('nvim-dap-virtual-text').setup {}
 
       vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
       vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
@@ -25,8 +107,9 @@ return {
       vim.keymap.set('n', '<F2>', dap.step_into)
       vim.keymap.set('n', '<F3>', dap.step_over)
       vim.keymap.set('n', '<F4>', dap.step_out)
-      vim.keymap.set('n', '<F5>', dap.step_back)
-      vim.keymap.set('n', '<F13>', dap.restart)
+      vim.keymap.set('n', '<F5>', dap.restart)
+      vim.keymap.set('n', '<F6>', dap.step_back)
+      vim.keymap.set('n', '<F12>', ui.toggle)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
